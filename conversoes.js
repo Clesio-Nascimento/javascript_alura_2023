5d2df16857964f93d35a8e3e37a507229d77854d
Clesio Nascimento <clesio.nascto@gmail.com>
2023-09-18 22:26:36 -0300

    Codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleanos
+
+// conversão inplícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString)
+
+console.log(numero + Number(numeroString))
+
+console.log(String(numero) + numeroString)
+
+// conversão explícita 
+//Number()
+//String()
