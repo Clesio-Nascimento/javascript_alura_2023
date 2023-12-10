8116e74c38d64148f9d36509bc8dc3dd7decaac2
Clesio Nascimento <clesio.nascto@gmail.com>
2023-12-10 16:10:19 -0300

    Exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,25 @@
+const texto1 = 'Olá Mundo!';
+const texto2 = "Olá, Mundo!";
+const senha = "senhaSuperSegura456";
+const StringDeNumeros = "34567";
+
+
+const citacao = "Meu nome é";
+const meuNome = "Clesio";
+
+const cifrao = '\u0024';
+
+// Concatenação (+)
+
+console.log(citacao, meuNome, cifrao);
+
+//template string OU template literal
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo);
+
+console.log(senha.length)
